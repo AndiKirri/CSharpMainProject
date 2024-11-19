0f210d2614ca27a27394c37114fa917bc9a71496
AndiKirri <dima.sobolevl.02@list.ru>
2024-11-19 23:29:38 +0300

    ДЗ_2_Дуга

---
+++ Assets/My_scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string s = "5";
+        string hello = "Hello world";
+        Debug.Log(hello + s);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
